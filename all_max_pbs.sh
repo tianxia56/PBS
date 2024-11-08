693ef69e3ae4a2c1329b1f426bd01db282203faa
tianxia56 <tian.xia.tx56@yale.edu>
2024-11-08 08:55:01 -0500

    Update all_max_pbs.sh

--- all_max_pbs.sh
+++ all_max_pbs.sh
@@ -1,4 +1,10 @@
 #!bin/bash
+#SBATCH --partition=day
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=102400
 
 bash make-max-afr-pbs.sh
 bash make-max-amr-pbs.sh
