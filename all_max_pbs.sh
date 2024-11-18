aa14c17f6b3b45f5cda3ca4726d4dcfeccda8dc2
tianxia56 <tian.xia.tx56@yale.edu>
2024-11-18 17:23:47 -0500

    Update all_max_pbs.sh

--- all_max_pbs.sh
+++ all_max_pbs.sh
@@ -5,10 +5,10 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=102400
-#SBATCH --exclusive
 
-sbatch --job-name=max-afr bash make-max-afr-pbs.sh
-sbatch --job-name=max-amr bash make-max-amr-pbs.sh
-sbatch --job-name=max-eas bash make-max-eas-pbs.sh
-sbatch --job-name=max-eur bash make-max-eur-pbs.sh
-sbatch --job-name=max-sas bash make-max-sas-pbs.sh
+
+bash make-max-afr-pbs.sh
+bash make-max-amr-pbs.sh
+bash make-max-eas-pbs.sh
+bash make-max-eur-pbs.sh
+bash make-max-sas-pbs.sh
